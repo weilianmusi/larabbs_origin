9b319b988d76ab3a55e734b2a7aedb824c41d5f5
vagrant <vagrant@homestead>
2018-12-28 14:14:33 +0000

    Seeding topics and users data

--- database/migrations/2018_12_28_203905_create_topics_table.php
+++ database/migrations/2018_12_28_203905_create_topics_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTopicsTable extends Migration 
+class CreateTopicsTable extends Migration
 {
 	public function up()
 	{
@@ -16,7 +16,7 @@ class CreateTopicsTable extends Migration
             $table->integer('reply_count')->unsigned()->default(0);
             $table->integer('view_count')->unsigned()->default(0);
             $table->integer('last_reply_user_id')->unsigned()->default(0);
-            $table->integer('order')->unsigned()->defaut(0);
+            $table->integer('order')->unsigned()->default(0);
             $table->text('excerpt')->nullable();
             $table->string('slug')->nullable();
             $table->timestamps();
