27f5976dc095d83febf213f3b680dac4bb988170
vagrant <vagrant@homestead>
2019-01-03 11:57:00 +0000

    Auto translate slug

--- resources/views/users/_topics.blade.php
+++ resources/views/users/_topics.blade.php
@@ -3,7 +3,7 @@
 <ul>
     @foreach ($topics as $topic)
         <li class="list-group-item">
-            <a href="{{ route('topics.show', $topic->id) }}">
+            <a href="{{ $topic->link() }}">
                 {{ $topic->title }}
             </a>
             <span class="meta pull-right">
