d328a260fa65892c21bc3b7673a4b284ce363c8f
vagrant <vagrant@homestead>
2019-01-06 11:15:35 +0000

    Admin can manage site settings

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
 		<p class="pull-left">
 			由<a href="http://www.baidu.com" target="_blank">Summer</a>设计和编码<span style="color: #e27575;font-size: 14px;">♥</span>
 		</p>
-		<p class="pull-right"><a href="2911@qq.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
 	</div>
 </footer>
